5f0a854d0d19ba36c2d6ca5e12ef3688525eb67c
calebcouperus <124112022+calebcouperus@users.noreply.github.com>
2023-03-30 11:45:02 +1300

    v1

---
+++ Yes-No_Checker_Lucky_Unicorn_v1.py
@@ -0,0 +1,14 @@
+# Ask the user if they have played before
+show_instructions = input("Have you played before?: ").lower()
+
+# If they say yes, output "Program Continues'
+if show_instructions == 'yes':
+    print("program continues")
+
+# If they say no, output "Display instructions'
+elif show_instructions == 'no':
+    print("Display instructions")
+
+# Otherwise - show error
+else:
+    print("Please answer 'yes' or 'no'")
