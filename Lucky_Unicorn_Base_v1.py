73ae7ded08822dbf34d7b512e0fefde3e01fadb8
calebcouperus <124112022+calebcouperus@users.noreply.github.com>
2023-04-04 09:58:42 +1200

    Lucky Unicorn Base v1 added

---
+++ Lucky_Unicorn_Base_v1.py
@@ -0,0 +1,43 @@
+"""Lucky Unicorn base component
+Components added after they have been created and tested
+"""
+
+
+# Yes/No checking function
+def yes_no(question_text):
+    while True:
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
+
+        # If they say yes, output "Program Continues'
+        if answer == 'yes' or answer == 'y':
+            answer = "Yes"
+            return answer
+
+        # If they say no, output "Display instructions'
+        elif answer == 'no' or answer == 'n':
+            answer = "No"
+            return answer
+
+        # Otherwise - show error
+        else:
+            print("Please answer 'yes' or 'no'")
+
+
+# Function to display instructions
+def instructions():
+    print("**** How to Play ****")
+    print()
+    print("The rules of the game will go here")
+    print()
+    print("Program continues")
+    print()
+
+
+# Main routine starts here
+played_before = yes_no("Have you played this game before?: ")
+
+if played_before == "No":
+    instructions()
+else:
+    print("Program continues")
